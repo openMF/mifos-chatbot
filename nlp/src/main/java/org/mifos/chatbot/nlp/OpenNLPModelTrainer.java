397fcce98c2012acaa56eec96361514cb9e09a7f
ZhaoDingfan <dingfan@Dingfans-MacBook-Pro.local>
2018-06-04 01:16:27 +0800

    Add trainer skeleton and training data skeleton

---
+++ nlp/src/main/java/org/mifos/chatbot/nlp/OpenNLPModelTrainer.java
@@ -0,0 +1,12 @@
+package org.mifos.chatbot.nlp;
+
+/**
+ * This class is used to train the named entity recognition, which is the model related to financial terminologies
+ * Ideas come from http://blog.thedigitalgroup.com/open-nlp-name-finder-model-training
+ */
+public class OpenNLPModelTrainer {
+    // about 15,000 sentences will be enough for the model to output satisfactory named entity.
+    public Boolean train() {
+        return null;
+    }
+}
