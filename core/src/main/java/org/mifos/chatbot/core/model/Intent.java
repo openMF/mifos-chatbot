bf377afc46fdc46fb0c2d82a59eb6d9cc4c3e4ee
ZhaoDingfan <dingfan@64-141.priv31.nus.edu.sg>
2018-06-09 18:45:07 +0800

    Update service code of OpenNLP

--- core/src/main/java/org/mifos/chatbot/core/model/Intent.java
+++ core/src/main/java/org/mifos/chatbot/core/model/Intent.java
@@ -3,4 +3,17 @@ package org.mifos.chatbot.core.model;
 // the data holder
 public class Intent {
     // Intent represents a mapping between what a user says and what action your software should take.
+    private String keyword;
+
+    public Intent(String keyword) {
+        this.keyword = keyword;
+    }
+
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
 }
--- nlp/src/main/java/org/mifos/chatbot/nlp/OpenNLPService.java
+++ nlp/src/main/java/org/mifos/chatbot/nlp/OpenNLPService.java
@@ -1,21 +1,111 @@
 package org.mifos.chatbot.nlp;
 
+import opennlp.tools.namefind.NameFinderME;
+import opennlp.tools.namefind.TokenNameFinderModel;
+import opennlp.tools.sentdetect.SentenceDetectorME;
+import opennlp.tools.sentdetect.SentenceModel;
+import opennlp.tools.tokenize.Tokenizer;
+import opennlp.tools.tokenize.TokenizerME;
+import opennlp.tools.tokenize.TokenizerModel;
+import opennlp.tools.util.Span;
 import org.mifos.chatbot.core.NLPService;
 import org.mifos.chatbot.core.model.Intent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class OpenNLPService implements NLPService {
 
+    Logger logger = LoggerFactory.getLogger(OpenNLPService.class);
+
+    /**
+     * This method is to recognise user's input to find out what is their intention
+     *
+     * @param text This is the message of user input, it may contain several sentences about their commands
+     * @return user's intention, basically what they really want to do
+     *
+     * @author Zhao Dingfan
+     */
     @Override
-    public Intent recognize(String text) {
+    public Intent[] recognize(String text) {
+        try {
+            String[] sentences = detectSentence(text);
+            System.out.println(sentences.length);
+
+            List<String> tokens = new ArrayList<>();
+            for(String sentence : sentences) {
+                tokens.addAll(Arrays.asList(tokenize(sentence)));
+            }
+
+            String[] tokenString = new String[tokens.size()];
+            tokenString = tokens.toArray(tokenString);
+            Span[] resultSpans = findName(tokenString);
+
+            Intent[] resultIntents = new Intent[resultSpans.length];
+            for(int i = 0; i < resultIntents.length ; i++) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(tokenString[resultSpans[i].getStart()]);
+                for(int j = resultSpans[i].getStart()+1 ; j < resultSpans[i].getEnd() ; j++) {
+                    sb.append(" ");
+                    sb.append(tokenString[j]);
+                }
+                resultIntents[i] = new Intent(sb.toString());
+            }
+
+            System.out.println(resultIntents[0].getKeyword());
+            return resultIntents;
+        } catch (IOException e) {
+            logger.error("Cannot read model information : ", e);
+        }
+
         return null;
     }
 
     // Refer to OpenNLP framework as much as I want
 
-    // OpenNLP may has to be trained by myself to be financial focused
     // May start with 10 keywords first, let framework understand different sentence structure.
 
+    private String[] detectSentence(String paragraph) throws IOException {
+        InputStream is = new FileInputStream("src/main/resources/models/en-sent.bin");
+        SentenceModel model = new SentenceModel(is);
+        SentenceDetectorME sdetector = new SentenceDetectorME(model);
 
+        String sentences[] = sdetector.sentDetect(paragraph);
+        for(String sentence : sentences) {
+            System.out.println(sentence);
+        }
+        is.close();
 
-    // create training set and training model by myself instead of using pre-trained model
+        return sentences;
+    }
+
+    private String[] tokenize(String sentence) throws IOException {
+        InputStream is = new FileInputStream("src/test/resources/models/en-token.bin");
+        TokenizerModel model = new TokenizerModel(is);
+        Tokenizer tokenizer = new TokenizerME(model);
+
+        String tokens[] = tokenizer.tokenize(sentence);
+        for (String a : tokens)
+            System.out.println(a);
+        is.close();
+
+        return tokens;
+    }
+
+    private Span[] findName(String[] tokens) throws IOException {
+        InputStream is = new FileInputStream("src/test/resources/models/en-ner-first-try.bin");
+        TokenNameFinderModel model = new TokenNameFinderModel(is);
+        NameFinderME nameFinder = new NameFinderME(model);
+
+        Span nameSpans[] = nameFinder.find(tokens);
+
+        is.close();
+        return nameSpans;
+    }
 }
