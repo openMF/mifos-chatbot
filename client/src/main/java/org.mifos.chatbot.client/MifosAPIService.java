3dc0f8f9cbf225738b6be531b9a07a9d5434490c
ZhaoDingfan <dingfan@Dingfans-MacBook-Pro.local>
2018-06-20 00:41:01 +0800

    Update file structure for client module

---
+++ client/src/main/java/org.mifos.chatbot.client/MifosAPIService.java
@@ -0,0 +1,15 @@
+package org.mifos.chatbot.client;
+
+import org.mifos.chatbot.core.MifosService;
+import org.mifos.chatbot.core.model.MifosRequest;
+import org.mifos.chatbot.core.model.MifosResponse;
+
+/**
+ * This class will handle the API interactions with Mifos
+ */
+public class MifosAPIService implements MifosService {
+    @Override
+    public MifosResponse process(MifosRequest request) {
+        return null;
+    }
+}
---
+++ client/src/test/java/org/mifos/chatbot/client/MifosAPIServiceTest.java
@@ -0,0 +1,10 @@
+package org.mifos.chatbot.client;
+
+import org.junit.Test;
+
+public class MifosAPIServiceTest {
+    @Test
+    public void processTest() {
+
+    }
+}
