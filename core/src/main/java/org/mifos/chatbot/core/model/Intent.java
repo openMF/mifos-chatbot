0a0933b4b87a4a37ec5573fe070d2a8b244086b5
ZhaoDingfan <dingfan@Dingfans-MacBook-Pro.local>
2018-06-19 21:44:38 +0800

    Update instructions for following development

--- core/src/main/java/org/mifos/chatbot/core/model/Intent.java
+++ core/src/main/java/org/mifos/chatbot/core/model/Intent.java
@@ -2,7 +2,10 @@ package org.mifos.chatbot.core.model;
 
 // the data holder
 public class Intent {
-    // Intent represents a mapping between what a user says and what action your software should take.
+    // Intent represents a mapping between what a user says and what action your Chatbot should take.
+
+    // provide the confidence level of the recognition
+    // When confidence level is lower than the threshold, then return error feedback
     private String keyword;
 
     public Intent(String keyword) {
@@ -16,4 +19,10 @@ public class Intent {
     public void setKeyword(String keyword) {
         this.keyword = keyword;
     }
+
+    // API category, function and parameters
+
+    // find mifos working on my own machine
+
+    // design mifos API client
 }
