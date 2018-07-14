88175693d6aeecbcf10c8d919c45c58dc335f947
dingfan <zhaodingfanhaha@gmail.com>
2018-07-14 15:59:53 +0900

    WIP

---
+++ adapter/src/main/java/org/mifos/chatbot/adapter/MifosChatbotAdapterService.java
@@ -0,0 +1,5 @@
+package org.mifos.chatbot.adapter;
+
+// @Service
+public class MifosChatbotAdapterService {
+}
---
+++ adapter/src/main/java/org/mifos/chatbot/adapter/handler/LoanStatusHandler.java
@@ -0,0 +1,6 @@
+package org.mifos.chatbot.adapter.handler;
+
+//@Slf4j
+//@Component
+public class LoanStatusHandler {
+}
