47a1854eebaf7a1e97e0e4ce3b689e86b25eff9d
iosdev747 <iosdev747@icloud.com>
2019-05-28 20:32:30 +0530

    remove redundant annotation with @SpringBootApplication

--- server/src/main/java/org/mifos/chatbot/server/MifosChatbotApplication.java
+++ server/src/main/java/org/mifos/chatbot/server/MifosChatbotApplication.java
@@ -16,16 +16,12 @@
 package org.mifos.chatbot.server;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 @ComponentScan(basePackages = "org.mifos.chatbot")
 @EnableCaching(proxyTargetClass = true)
 @EnableAspectJAutoProxy(proxyTargetClass = true)
