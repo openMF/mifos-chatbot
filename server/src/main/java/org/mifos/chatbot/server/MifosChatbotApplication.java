aa4d0c4ef47d17e1d7649e9330aa289e5864a85a
iosdev747 <iosdev747@icloud.com>
2019-05-29 15:14:34 +0530

    Add messenger4j dependency

--- server/src/main/java/org/mifos/chatbot/server/MifosChatbotApplication.java
+++ server/src/main/java/org/mifos/chatbot/server/MifosChatbotApplication.java
@@ -1,23 +1,26 @@
 /**
  * Copyright 2018 Dingfan Zhao
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.mifos.chatbot.server;
 
+import com.github.messenger4j.Messenger;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
@@ -26,6 +29,13 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @EnableCaching(proxyTargetClass = true)
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 public class MifosChatbotApplication {
+	@Bean
+	public Messenger messenger(@Value("${messenger4j.pageAccessToken}") String pageAccessToken,
+							   @Value("${messenger4j.appSecret}") final String appSecret,
+							   @Value("${messenger4j.verifyToken}") final String verifyToken) {
+		return Messenger.create(pageAccessToken, appSecret, verifyToken);
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(MifosChatbotApplication.class, args);
 	}
