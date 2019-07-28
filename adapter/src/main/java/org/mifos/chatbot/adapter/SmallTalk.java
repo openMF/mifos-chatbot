6b7a9ed68128b0bc88218f2178851571b6ad5440
iosdev747 <iosdev747@icloud.com>
2019-07-28 15:18:27 +0530

    Implement SmallTalkHandler and Update SmallTalk class

--- adapter/src/main/java/org/mifos/chatbot/adapter/SmallTalk.java
+++ adapter/src/main/java/org/mifos/chatbot/adapter/SmallTalk.java
@@ -3,7 +3,7 @@ package org.mifos.chatbot.adapter;
 import org.mifos.chatbot.core.model.Intent;
 
 class SmallTalk {
-    private static final String[] SMALL_TALK_INTENTS = {"hello", "bye"};
+    private static final String[] SMALL_TALK_INTENTS = {"acquaintance", "age", "bad_bot", "help", "chatbot", "good_bot", "origin", "sure", "thank_you", "welcome", "well_done", "greet_bye", "greet_goodevening", "greet_goodmorning", "greet_goodnight", "greet_hello", "greet", "angry", "happy", "sad", "testing"};
 
     static boolean isSmallTalkRequest(Intent intent) {
         if(intent == null) {
---
+++ adapter/src/main/java/org/mifos/chatbot/adapter/handler/smallTalk/SmallTalkHandler.java
@@ -0,0 +1,43 @@
+package org.mifos.chatbot.adapter.handler.smallTalk;
+
+import lombok.extern.slf4j.Slf4j;
+import org.mifos.chatbot.core.model.Intent;
+import org.mifos.chatbot.core.model.MifosResponse;
+import org.springframework.stereotype.Component;
+
+import java.util.Random;
+
+@Slf4j
+@Component
+public class SmallTalkHandler extends BaseSmallTalkHandler {
+    private static final String[] INTENT_KEYWORDS = {"greet_bye", "greet_hello"};
+    private static final String[][] GREET_RESPONSE = {
+            {"Bye Bye", "I'll see you later", "Bye, Have a nice day", "Goodbye", "Nice to talk to you!", "Nice to meet you!", "I'll miss you :'("},
+            {"Hello", "Hola", "Hi", "Hi there, friend!", "Hey!"}
+    };
+
+
+    @Override
+    public Boolean canHandle(Intent intent) {
+        boolean find = false;
+        for(String intent_keyword : INTENT_KEYWORDS) {
+            if (intent.getKeyword().toLowerCase().equals(intent_keyword.toLowerCase())) {
+                find = true;
+            }
+        }
+        return find;
+    }
+
+    @Override
+    public MifosResponse handle(Intent intent) {
+        String intentKeyword = intent.getKeyword().toLowerCase();
+        MifosResponse mifosResponse = new MifosResponse();
+        for(int i=0; i<INTENT_KEYWORDS.length; i++) {
+            if(intentKeyword.equals(INTENT_KEYWORDS[i])) {
+                mifosResponse.setContent((GREET_RESPONSE[i][new Random().nextInt(GREET_RESPONSE[i].length)]));
+                return mifosResponse;
+            }
+        }
+        return null;
+    }
+}
