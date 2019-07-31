04447f68b11aef61b11798edf90e4c6e434a91ef
iosdev747 <iosdev747@icloud.com>
2019-07-31 11:48:14 +0530

    Increase and add response to intents defined in SmallTalkHandler

--- adapter/src/main/java/org/mifos/chatbot/adapter/SmallTalk.java
+++ adapter/src/main/java/org/mifos/chatbot/adapter/SmallTalk.java
@@ -3,7 +3,28 @@ package org.mifos.chatbot.adapter;
 import org.mifos.chatbot.core.model.Intent;
 
 class SmallTalk {
-    private static final String[] SMALL_TALK_INTENTS = {"greet_bye", "greet_goodevening", "greet_goodmorning", "greet_goodnight", "greet_hello"};
+    private static final String[] SMALL_TALK_INTENTS = {
+            "acquaintance",
+            "age",
+            "bad_bot",
+            "help",
+            "chatbot",
+            "good_bot",
+            "origin",
+            "sure",
+            "thank_you",
+            "welcome",
+            "well_done",
+            "greet_bye",
+            "greet_goodevening",
+            "greet_goodmorning",
+            "greet_goodnight",
+            "greet_hello",
+            "angry",
+            "happy",
+            "sad",
+            "testing"
+    };
 
     static boolean isSmallTalkRequest(Intent intent) {
         if(intent == null) {
